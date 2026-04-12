7ec3f392a61b7e47a76e4048b86660d7350316bd
EthanWang <2918954597@qq.com>
2026-04-12 05:12:39 +0000

    Delete intall log build

--- ros2_ws/build/my_robot/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/dev_ws/AUG/ros2_ws/install/substation_sim/share/substation_sim/package.sh"
--- ros2_ws/build/substation_sim/CMakeFiles/substation_sim_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for substation_sim_uninstall.
